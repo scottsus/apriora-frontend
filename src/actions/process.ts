65905d3c966fb762fb6739745766d58c8c901b0f
scott susanto <susantoscott@gmail.com>
2024-09-07 23:58:15 -0700

    resolve eslint

--- src/actions/process.ts
+++ src/actions/process.ts
@@ -1,25 +1,21 @@
 "use server";
 
+/**
+ * TODO: Make a webhook?
+ */
 export async function processRecordingOffline({
   interviewId,
 }: {
   interviewId: number;
-}) {
+}): Promise<void> {
   const ffmpegProcessingUrl = process.env.FFMPEG_PROCESSING_URL ?? "url";
 
-  const response = await fetch(
+  await fetch(
     `${ffmpegProcessingUrl}/process-multimedia?interview_id=${interviewId}`,
     {
       method: "POST",
     },
   );
 
-  if (!response.ok) {
-    throw new Error(
-      `Failed to process recording for interview ID ${interviewId}`,
-    );
-  }
-
-  const result = await response.json();
-  return result;
+  return;
 }
